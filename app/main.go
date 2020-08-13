ead6912ba0f0aa51d46cca69bd5517697173ac9d
criscola <cristiano.colangelo@bluewin.ch>
2020-08-13 15:46:49 +0200

    Add pull of raindrops having a given label from Raindrop API

--- app/main.go
+++ app/main.go
@@ -2,28 +2,41 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"io/ioutil"
 	"net/http"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// Load configuration
+	viper.SetConfigName("secret")
+	viper.SetConfigName("preferences")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
 
 	client := &http.Client{}
-	
-	req, _ := http.NewRequest("GET", "https://api.raindrop.io/rest/v1/collections", nil)
-	req.Header.Set("Authorization", "Bearer aabc506e-5688-496e-80c2-fe8f6f185f2b")
-	res, _ := client.Do(req)
 
-	data, _ := ioutil.ReadAll(res.Body)
-	res.Body.Close()
+	req, err := http.NewRequest("GET", "https://api.raindrop.io/rest/v1/raindrops/0", nil)
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error in building request: %s \n", err))
+	}
+	// Raindrop API doesn't rigorously respect RFC 3986 so commas shouldn't be encoded, Go will always encode them so
+	// the URL encoding must be handled specifically for this case
+	req.URL.Opaque = `/rest/v1/raindrops/0?search=%5B%7B%22key%22:%22tag%22,%22val%22:%22` +
+		viper.GetString("LABEL_NAME") + `%22%7D%5D`
 
-	fmt.Printf("%s\n", data)
-	r.Run() // listen and serve on 0.0.0.0:8080
+	req.Header.Add("Authorization", "Bearer 6da4f2c4-f5bb-4626-9fb2-733e743fac1a")
+
+	res, err := client.Do(req)
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error sending request: %s \n", err))
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(string(body))
 }
