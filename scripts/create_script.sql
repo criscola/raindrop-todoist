5e521f28e08ba48ce6891239d054e0ac7633144d
Cristiano Colangelo <cristiano.colangelo@bluewin.ch>
2020-08-21 17:23:46 +0200

    Add unique constraint to id_task

--- scripts/create_script.sql
+++ scripts/create_script.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS bookmark_with_todo (
     id_bookmark bigint PRIMARY KEY,
-    id_todo bigint,
+    id_todo bigint UNIQUE,
     insert_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
